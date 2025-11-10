8593d93887d1900f31a2fb930d3ab4f8e2d0014b
SeviolaIS <seviolasafitri939@gmail.com>
2025-11-10 19:43:00 +0700

    Implement LED control tasks for ESP32

--- ESP32/LED/led.ino
+++ ESP32/LED/led.ino
@@ -1,1 +1,56 @@
-////
+// 3 LED, masing-masing task sendiri (pin sesuai gambar)
+// merah = 2, hijau = 4, biru = 5
+// merah -> Core 0 ; hijau & biru -> Core 1 (task terpisah)
+
+#define LED_RED   2
+#define LED_GREEN 4
+#define LED_BLUE  5
+
+void TaskLED_Red(void *pvParameters) {
+  pinMode(LED_RED, OUTPUT);
+  for (;;) {
+    digitalWrite(LED_RED, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(300));
+    digitalWrite(LED_RED, LOW);
+    vTaskDelay(pdMS_TO_TICKS(300));
+  }
+}
+
+void TaskLED_Green(void *pvParameters) {
+  pinMode(LED_GREEN, OUTPUT);
+  for (;;) {
+    digitalWrite(LED_GREEN, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(500));
+    digitalWrite(LED_GREEN, LOW);
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+
+void TaskLED_Blue(void *pvParameters) {
+  pinMode(LED_BLUE, OUTPUT);
+  for (;;) {
+    digitalWrite(LED_BLUE, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(700));
+    digitalWrite(LED_BLUE, LOW);
+    vTaskDelay(pdMS_TO_TICKS(700));
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+  Serial.println("ESP32-S3: 3 LED tasks (Red=2, Green=4, Blue=5)");
+
+  // Merah -> Core 0
+  xTaskCreatePinnedToCore(TaskLED_Red, "LED_Red", 2048, NULL, 1, NULL, 0);
+
+  // Hijau -> Core 1
+  xTaskCreatePinnedToCore(TaskLED_Green, "LED_Green", 2048, NULL, 1, NULL, 1);
+
+  // Biru -> Core 1
+  xTaskCreatePinnedToCore(TaskLED_Blue, "LED_Blue", 2048, NULL, 1, NULL, 1);
+}
+
+void loop() {
+  // kosong: semua dikerjakan oleh task
+}
